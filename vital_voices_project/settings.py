b9387477d9d31d73c1bf52a4c3ab6141e9f8dcf4
Vraj Nena <vnen0001@student.monash.edu>
2024-08-30 07:31:14 +1000

    final

--- vital_voices_project/settings.py
+++ vital_voices_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 # Add Azure's domain to ALLOWED_HOSTS
 
